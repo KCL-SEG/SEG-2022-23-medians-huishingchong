9524c63cba3a636fa0e96162a8d611c817a59bee
Hui Shing Chong <hui.chong@kcl.ac.uk>
2022-09-28 12:39:11 +0100

    Altered my solution for medians.py

--- median.py
+++ median.py
@@ -8,12 +8,14 @@ while True:
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
-        numbers.sort() #sort the list
-        if len(numbers) > 1:
-            median_index = len(numbers)//2
-            if len(numbers) % 2 != 0: #odd amount of numbers in list
-                print(numbers[median_index])
-            else:
-                print((numbers[median_index-1] + numbers[median_index])/2)
-        else:
-            print(numbers[0])
+        break
+
+numbers.sort() #sort the list
+if len(numbers) > 1:
+    median_index = len(numbers)//2
+    if len(numbers) % 2 != 0: #odd amount of numbers in list
+        print(numbers[median_index])
+    else:
+        print((numbers[median_index-1] + numbers[median_index])/2)
+else:
+    print(numbers[0])
